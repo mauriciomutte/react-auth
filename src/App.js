2dc70f994e13d59e1739ebe0d126905cfbebf55e
Mauricio Mutte <litiobr@gmail.com>
2019-12-09 17:11:09 -0300

    Feat: add Route to Admin and Home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Admin from './pages/Admin';
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home Page</Link>
+          </li>
+          <li>
+            <Link to="/admin">Admin Page</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/admin" component={Admin} />
+      </div>
+    </Router>
   );
 }
 
