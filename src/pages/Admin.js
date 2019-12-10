27c000f3e3fe682a907e601af87f81dc37c6011d
Mauricio Mutte <litiobr@gmail.com>
2019-12-09 23:12:43 -0300

    Feat: add token and logOut function

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import { Button } from '../components/AuthForm';
+import { useAuth } from '../context/auth';
 
 export default function Admin(props) {
+  const { setAuthTokens } = useAuth();
+
+  function logOut() {
+    setAuthTokens();
+  }
+
   return (
     <div>
       <div>Admin Page</div>
-      <Button>Log out</Button>
+      <Button onClick={logOut}>Log out</Button>
     </div>
   );
 }
