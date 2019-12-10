f089ceca1fa9ec11cd21854c8a37bfab08b215a4
Mauricio Mutte <litiobr@gmail.com>
2019-12-09 23:16:44 -0300

    Feat: add AuthContext Provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Admin from './pages/Admin';
+import { AuthContext } from './context/auth';
 
-function App() {
+function App(props) {
+  const [authTokens, setAuthTokens] = useState();
+
+  const setTokens = data => {
+    localStorage.setItem("token", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home Page</Link>
-          </li>
-          <li>
-            <Link to="/admin">Admin Page</Link>
-          </li>
-        </ul>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens}}>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Page</Link>
+            </li>
+          </ul>
 
-        <Route exact path="/" component={Home} />
-        <Route exact path="/admin" component={Admin} />
-      </div>
-    </Router>
+          <Route exact path="/" component={Home} />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
