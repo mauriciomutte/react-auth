6a5ac05efb741a9c0cf0fc81614d6644946e78b0
Mauricio Mutte <litiobr@gmail.com>
2019-12-12 12:23:18 -0300

    Feat(server): add server.js

---
+++ packages/server/src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+dotenv.config();
+
+// Database
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+// Middlewares & Routes
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// Server
+app.listen(3030);
