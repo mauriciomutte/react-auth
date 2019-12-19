f30427bc50cca6d8af2df435a771b0d48e3ef014
Mauricio Mutte <litiobr@gmail.com>
2019-12-19 14:08:36 -0300

    Feat(web): add Profile route

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import PrivateRoute from './PrivateRoute';
@@ -12,6 +13,7 @@ function App() {
 			<Route path="/login" component={Login} />
 			<Route path="/signup" component={Signup} />
 			<PrivateRoute exact path="/" component={Home} />
+			<PrivateRoute exact path="/profile" component={Profile} />
 		</BrowserRouter>
   );
 }
