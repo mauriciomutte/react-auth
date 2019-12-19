c2d5a78869012d65649b3f7e30a22cbb096610b4
Mauricio Mutte <litiobr@gmail.com>
2019-12-18 23:19:35 -0300

    Feat(web): create Menu component

---
+++ packages/web/src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import * as S from './style';
+
+import MenuItem from '../MenuItem';
+
+export default () => {
+	return(
+		<S.Menu>
+			<S.MenuLogo>Company <span role="img" aria-label="Building Construction">🏗️</span></S.MenuLogo>
+			<S.MenuNav>
+				<ul>
+					<MenuItem name="Home" link="/"/>
+					<MenuItem name="Sobre" link="/"/>
+				</ul>
+			</S.MenuNav>
+		</S.Menu>
+	);
+}
---
+++ packages/web/src/components/Menu/style.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const Menu = styled.div`
+	background-color: #6371c7;
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	width: 15%;
+	padding: 20px;
+`;
+
+export const MenuLogo = styled.nav`
+	font-size: 26px;
+	text-align: center;
+	color: #fff;
+	padding: 10px 0;
+	font-weight: bold;
+`;
+
+export const MenuNav = styled.nav`
+	padding: 10px 0;
+	margin: 20px 0;
+	border-top: 1px solid #fff5;
+	border-bottom: 1px solid #fff5;
+`;
