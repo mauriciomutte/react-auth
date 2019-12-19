e9cb79c7a84af7ab133d1c3c782619e855b83209
Mauricio Mutte <litiobr@gmail.com>
2019-12-18 23:53:09 -0300

    Feat(web): create Header

---
+++ packages/web/src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SessionUser from '../SessionUser';
+
+const Header = styled.div`
+	display: flex;
+	padding: 10px 20px;
+	align-items: centerPageTitle
+PitocoPageTitle
+Pitoco;
+	justify-content: space-between;
+
+	h1 {
+		color: #6371c7;
+		font-weight: bold;
+		font-size: 32px;
+	}
+`;
+
+export default ({ pageTitle, userName, userImg }) => {
+	return (
+		<Header>
+			<h1>{pageTitle}</h1>
+			<SessionUser
+				name={userName}
+				img={userImg}
+			/>
+		</Header>
+	);
+}
