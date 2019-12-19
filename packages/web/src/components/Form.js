502196c79af0f4976428efa887aa71ff554a071e
Mauricio Mutte <litiobr@gmail.com>
2019-12-19 20:24:22 -0300

    Feat(web): add Form component

---
+++ packages/web/src/components/Form.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+	display: flex;
+	background-color: #fff;
+	border-radius: 10px;
+	flex-direction: column;
+	max-width: 700px;
+	margin: 3em auto 1em;
+	padding: 2em 3em;
+`;
+
+export const ProfileImage = styled.div`
+	margin-bottom: 2em;
+
+	img {
+		border-radius: 50%;
+		height: 100px;
+		width: 100px;
+	}
+`;
+
+export const Label = styled.label`
+	color: #6371c7;
+	font-weight: bold;
+	font-size: 18px;
+	margin-bottom: 5px;
+`;
+
+export const InputText = styled.input`
+	margin-bottom: 2em;
+	border: 2px solid #0003;
+	padding: 10px;
+	border-radius: 2px;
+`;
+
+export const Submit = styled.button`
+	background: #6371c7;
+	border: none;
+	border-radius: 5px;
+	color: #fff;
+	font-size: 16px;
+	font-weight: bold;
+	margin-left: auto;
+	padding: 7px 14px;
+	width: 120px;
+`;
--- packages/web/src/pages/Profile.js
+++ packages/web/src/pages/Profile.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Menu from '../components/Menu';
 import Main from '../components/Main';
 import Header from '../components/Header';
+import { Form, Label, InputText, ProfileImage, Submit } from '../components/Form';
 
 export default function Profile() {
+  const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+
   return (
 		<div>
 			<Menu />
@@ -13,6 +17,29 @@ export default function Profile() {
 					pageTitle="Profile"
 					userImg="https://ddragon.leagueoflegends.com/cdn/9.24.2/img/champion/Ahri.png"
 				/>
+				<Form>
+					<Label>Your photo:</Label>
+					<ProfileImage>
+						<img src="https://ddragon.leagueoflegends.com/cdn/9.24.2/img/champion/Ahri.png" alt="" />
+						<input type="file" />
+					</ProfileImage>
+
+					<Label>Name:</Label>
+					<InputText
+						type="text"
+						value={name}
+						onChange={e => { setName(e.target.value) }}
+					/>
+
+					<Label>Email:</Label>
+					<InputText
+						type="email"
+						value={email}
+						onChange={e => { setEmail(e.target.value) }}
+					/>
+
+					<Submit type="submit" onClick={updateProfile}>Submit</Submit>
+				</Form>
 			</Main>
 		</div>
   );
