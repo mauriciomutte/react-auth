0d8dd4c47a319ce98cbc69f1cef5661657976b23
Mauricio Mutte <litiobr@gmail.com>
2019-12-18 23:18:55 -0300

    Feat(web): create MenuItem component

---
+++ packages/web/src/components/MenuItem/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MenuItem = styled.li`
+	margin: 8px 0;
+	border-radius: 3px;
+	background-color: #fff2;
+
+	a {
+		color: #fff;
+		display: block;
+		font-weight: bold;
+		text-decoration: none;
+		padding: 12px;
+	}
+
+	:hover {
+		background-color: #fff4;
+	}
+`;
+
+export default ({ name, link }) => {
+	return (
+		<MenuItem>
+			<a href={link}>{name}</a>
+		</MenuItem>
+	);
+}
